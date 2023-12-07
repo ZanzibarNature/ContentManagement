22ff6ab0ddf337550b03e3f3a44e005fd8dffaf0
662781 <662781@student.inholland.nl>
2023-12-07 15:54:57 +0100

    Update ILocationController.cs

--- ContentManagement/ContentAPI/Controller/Interfaces/ILocationController.cs
+++ ContentManagement/ContentAPI/Controller/Interfaces/ILocationController.cs
@@ -7,7 +7,7 @@ namespace ContentAPI.Controller.Interfaces
     public interface ILocationController
     {
         Task<IActionResult> GetLocationByKey(string partitionKey, string rowKey);
-        Task<IActionResult> CreateLocation([FromBody] CreateLocationDTO locationDTO,[FromForm] IFormFile? bannerImage,[FromForm] IFormFileCollection? additionalImages);
+        Task<IActionResult> CreateLocation([FromBody] CreateLocationDTO locationDTO, [FromForm] IFormFile? bannerImage, [FromForm] IFormFileCollection? additionalImages);
         Task<IActionResult> UpdateLocation([FromBody] Location location, [FromForm] IFormFile? bannerImage, [FromForm] IFormFileCollection? additionalImages);
         Task<IActionResult> DeleteLocation(string partitionKey, string rowKey);
     }
